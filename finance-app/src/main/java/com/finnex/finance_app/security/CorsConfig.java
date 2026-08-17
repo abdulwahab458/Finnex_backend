644450694854a08ecbeab151d00860b6e00d9a61
abdulwahab458 <abdulwahab330066@gmail.com>
2026-08-17 23:11:30 +0530

    fixed cors issue

--- finance-app/src/main/java/com/finnex/finance_app/security/CorsConfig.java
+++ finance-app/src/main/java/com/finnex/finance_app/security/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.finnex.finance_app.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -11,13 +12,15 @@ import java.util.List;
 @Configuration
 public class CorsConfig {
 
+    @Value("${app.cors.allowed-origins}")
+    private String allowedOrigins;
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
 
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.setAllowedOrigins(
-                List.of("http://localhost:5173")
+                List.of(allowedOrigins)
                 
         );
 
